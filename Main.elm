bbec43b16b0166f9a95130f8eb87068a4e352378
Rohit Maharjan <Ro@192-168-1-28.tpgi.com.au>
2018-09-14 15:14:32 +1000

    A simple Counter to test the view and update features in elm

--- Main.elm
+++ Main.elm
@@ -1,15 +1,45 @@
-module Main exposing (main)
+import Browser
 
-import Html
-
-
-add a b =
-    a + b
-
-
-result =
-    add 2 2 |> \a -> modBy 2 a == 0
-
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 main =
-    Html.text (Debug.toString result)
+  Browser.sandbox { init = initModel, update = update, view = view }
+
+--Model
+
+type alias Model =
+    Int
+
+initModel : Model
+
+initModel =
+    0
+
+--Update
+
+type Msg = 
+    AddCalcorie
+    | Clear
+
+update: Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddCalcorie ->
+            model + 1
+
+        Clear ->
+            initModel
+
+--View //Create View in Html
+
+view: Model -> Html Msg
+view model =
+    div []
+        [
+            h3  [][text ("Counter: " ++ (Debug.toString model)) ]
+
+            ,button [type_ "button", onClick AddCalcorie][text "Add"]
+            ,button [type_ "button", onClick Clear][text "Clear"]
+        ]
